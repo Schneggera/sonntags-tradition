831cdfe80c11220389b7742e38eb0ddf40296e96
Tobias Reichel <top.20mail@gmail.com>
2020-12-12 00:40:28 +0100

    added insert of recipe

--- php/insert.php
+++ php/insert.php
@@ -1,5 +1,29 @@
 <?php
 
-file_put_contents ('test.txt', 'test');
+$arr = array(
+        "title" => $_POST['title'],  
+        "ingredient" => $_POST['ingredients'],
+        "recipe" => $_POST['recipe']
+    );
+
+$inp = file_get_contents('results.json');
+$tempArray = json_decode($inp);
+array_push($tempArray, $arr);
+$jsonData = json_encode($tempArray);
+file_put_contents('results.json', $jsonData);
+
+//$recipeJson = json_encode($recipe);
+
+//$inp = file_get_contents('results.json');
+//$inp .= $recipeJson;
+
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo $output;
+}
+debug_to_console($jsonData);
 
 ?>
